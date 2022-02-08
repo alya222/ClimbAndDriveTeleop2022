df238e2a55546fcee337365184f9e2aafeac5c60
alya222 <alyanimisibrahim@gmail.com>
2022-02-08 16:11:08 -0600

    removed unnecessary import

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,7 +16,6 @@ import static edu.wpi.first.wpilibj.XboxController.Button.*;
 // import commands
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.Climb;
 
